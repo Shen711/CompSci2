a7a4f83d10ec8cb929b66eafa737173401837faa
Nathan Schoenike <natschoenike711@gmail.com>
2025-10-30 15:26:42 +0000

    Finished setting up all classes

---
+++ M1P1.cpp
@@ -0,0 +1,110 @@
+
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <limits.h>
+
+using namespace std;
+
+class Cube
+{
+    int length;
+    int width;
+    int height;
+    int volume;
+    string color;
+
+    public:
+    
+     int getWidth()
+    {
+        return width;
+    }
+    
+    int getHeight()
+    {
+        return height;
+    }
+    
+    int getLength()
+    {
+        return length;
+    }
+    
+    void setHeight()
+    {
+        int cubeHeight;
+        cout << "What is the height of the cube in inches? " << endl;
+        do
+        {
+            cin >> cubeHeight;
+            if (!cin || cubeHeight <= 0)
+            {
+                cout << "Enter a valid positive integer. " << endl;
+                cin.clear();
+                cin.ignore(INT_MAX, '\n');
+            }
+        }while(cubeHeight <= 0);
+        
+        height = cubeHeight;
+        
+    }
+
+    void setLength()
+    {
+        int cubeLength;
+        cout << "What is the length of the cube in inches? " << endl;
+        do
+        {
+            cin >> cubeLength;
+            if (!cin || cubeLength <= 0)
+            {
+                cout << "Enter a valid positive integer. " << endl;
+                cin.clear();
+                cin.ignore(INT_MAX, '\n');
+            }
+        }while(cubeLength <= 0);
+        
+        height = cubeLength;
+        
+    }
+
+    void setWidth()
+    {
+        int cubeWidth;
+        cout << "What is the width of the cube in inches? " << endl;
+        do
+        {
+            cin >> cubeWidth;
+            if (!cin || cubeWidth <= 0)
+            {
+                cout << "Enter a valid positive integer. " << endl;
+                cin.clear();
+                cin.ignore(INT_MAX, '\n');
+            }
+        }while(cubeWidth <= 0);
+        
+        height = cubeWidth;
+        
+    }
+
+    void calculateVolume()
+    {
+        volume = length * width * height;
+    }
+};
+
+int main() {
+    
+    Cube self;
+
+    cout << "Hello." << endl;
+
+    
+    self.setHeight();
+    
+    
+    
+
+    return 0;
+}
