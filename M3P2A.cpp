df450a68697c86401e25a98e55956c382a42f833
Nathan Schoenike <natschoenike711@gmail.com>
2025-11-17 20:52:53 +0000

    M3P2A done

--- M3P2A.cpp
+++ M3P2A.cpp
@@ -1,3 +1,8 @@
+//Program Name: Non-Duplicate Words
+//Author: Nathan Schoenike
+//Date Last Updated: 11/17/2025
+//Purpose: To read in a file and display the words in the file in ascending order
+
 #include <fstream> //for file
 #include <set> //for assignment
 #include <limits>
@@ -33,10 +38,19 @@ int main()
 
     cout << Siz << endl;
 
-    for (int i = 0; i < Siz; i++)
+    //Words automatically sort in ascending order of letter in word
+    cout << "Words in ascending order: " << endl;
+    for (auto word : nonduplicate)
     {
-        cout << i << endl;
+        cout << word << endl;
     }
+
+    wordFile.close();
+
+    return 0;
+
+
+    
 
 
 }
